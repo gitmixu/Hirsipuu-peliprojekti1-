9d0a85446d04612b4bd264731f45d08c0462429a
Arto Eskelinen <arto.eske@gmail.com>
2023-03-22 10:37:12 +0200

    "ohjeen" lisäys

--- hirsipuu.py
+++ hirsipuu.py
@@ -3,6 +3,14 @@ class Hirsipuu:
     def piirto(self, arvo: int):
         pass #esim arvo 1 piirtää hirsipuun, 2 sanan, jne
 
+    def ohje(self, arvo: int):
+        if arvo == 1:
+            print("Hirsipuu") #aloitusohjeet
+            print("1 - aloita peli, 2 - katsele historiaa, 3 - lopeta")
+        elif arvo == 2:
+            #pelin aikaiset ohjeet
+            print("Kirjoita kirjain: ")
+
     def pelaa():
         pass
     
